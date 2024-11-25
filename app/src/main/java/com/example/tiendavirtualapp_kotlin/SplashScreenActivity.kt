aac49d115f6d82a88f71967d4e39077da6edf342
Matias-sh <devmatiasbritez@gmail.com>
2024-11-25 12:35:36 -0300

    first commit

---
+++ app/src/main/java/com/example/tiendavirtualapp_kotlin/SplashScreenActivity.kt
@@ -0,0 +1,38 @@
+package com.example.tiendavirtualapp_kotlin
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.CountDownTimer
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        verBienvenida()
+    }
+
+    private fun verBienvenida() {
+        object : CountDownTimer(3000, 1000){
+            override fun onTick(millisUntilFinished: Long) {
+
+            }
+
+            override fun onFinish() {
+                startActivity(Intent(applicationContext, MainActivity::class.java))
+                finishAffinity()
+            }
+
+        }.start()
+    }
+}
